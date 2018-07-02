46ce8c27bcc313eb9f02cecd90a8fc707798c93c
Peter Mount <peter@retep.org>
2018-07-02 12:39:29 +0100

    Fix if test

--- test/control_test.go
+++ test/control_test.go
@@ -3,45 +3,55 @@ package test
 import (
   "github.com/peter-mount/calculator/calculator"
   "github.com/peter-mount/calculator/context"
-  "github.com/peter-mount/calculator/debug"
-  "os"
   "testing"
 )
 
-// Test basic math precedence
 func TestIf( t *testing.T ) {
-
-  f, err := os.OpenFile( "/tmp/if.html", os.O_CREATE | os.O_TRUNC|os.O_WRONLY, 0666 )
-  if err != nil {
-    t.Error( err )
-    return
-  }
-  defer f.Close()
-
-  debug.HtmlTreeStart( f )
 
   calc := &calculator.Calculator{}
 
-  e := "$a=-1;if 1 { $a=1 ; 42}"
+  e := "$a=-1;if $a == -1 { $a=1 }"
 
-  err = calc.ParseScriptString( e )
+  err := calc.ParseScriptString( e )
   if err != nil {
     t.Error( err )
   }
-
-  debug.HtmlTree( calc.GetRoot(), f, e )
 
   ctx := &context.Context{}
   err = calc.Execute( ctx )
   if err != nil {
     t.Error( err )
   }
-  f.WriteString( "<p><strong>" )
-  f.WriteString( e )
-  f.WriteString( "</strong> = ")
-  debug.StackDump( f, ctx )
-  debug.VarDump( f, ctx )
-  f.WriteString( "</p> ")
 
-  debug.HtmlTreeEnd( f )
+  a := ctx.GetVar( "a" )
+  if a == nil {
+    t.Error( "No output" )
+  } else if !a.IsNumeric() || a.Int() != 1 {
+    t.Errorf( "Expected 1 got %v", a )
+  }
+}
+
+func TestIfElse( t *testing.T ) {
+
+  calc := &calculator.Calculator{}
+
+  e := "$a=-1;if $a != -1 { $a=1 } else { $a=2 }"
+
+  err := calc.ParseScriptString( e )
+  if err != nil {
+    t.Error( err )
+  }
+
+  ctx := &context.Context{}
+  err = calc.Execute( ctx )
+  if err != nil {
+    t.Error( err )
+  }
+
+  a := ctx.GetVar( "a" )
+  if a == nil {
+    t.Error( "No output" )
+  } else if !a.IsNumeric() || a.Int() != 2 {
+    t.Errorf( "Expected 2 got %v", a )
+  }
 }
